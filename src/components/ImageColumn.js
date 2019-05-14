e17ddd2170ff467846e24967c67df43f0dc8ff6a
Wilton Ngo <wiltonngo@gmail.com>
2019-05-14 16:10:55 -0400

    Image column now renders their own images

---
+++ src/components/ImageColumn.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { ImageCard } from "./ImageCard";
+import "./ImageColumn.css";
+
+export const ImageColumn = ({ images }) => {
+  const imgCards = images.map(image => {
+    const { id, alt_description, urls } = image;
+    return (
+      <ImageCard
+        alt={alt_description}
+        key={id}
+        src={urls.regular}
+        image={image}
+      />
+    );
+  });
+  return <div className="img__col">{imgCards}</div>;
+};
