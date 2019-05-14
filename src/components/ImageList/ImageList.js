85beb1a12524c17d83a598909b2778857bd68da9
Wilton Ngo <wiltonngo@gmail.com>
2019-05-14 17:59:56 -0400

    Removed comments

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -1,21 +1,10 @@
-import "./ImageList.css";
-
-import { ImageColumn } from "../ImageColumn/ImageColumn";
 import React from "react";
+import { ImageColumn } from "../ImageColumn/ImageColumn";
+import "./ImageList.css";
 
 export const ImageList = ({ columns }) => {
   const cols = columns.map((column, index) => (
     <ImageColumn key={`col-${index}`} images={column} />
   ));
   return <div className="image-list">{cols}</div>;
-  /*
-  const images = props.images.map(image => {
-    const { id, description, urls } = image;
-    return (
-      <ImageCard alt={description} key={id} src={urls.regular} image={image} />
-    );
-  });
-  
-  return <div className="image-list" />;
-  */
 };
