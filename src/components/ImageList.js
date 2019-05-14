8b25e10d8e74a2df86c99ab4de34e7a9902d39e0
Wilton Ngo <wiltonngo@gmail.com>
2019-05-14 16:11:40 -0400

    Modified presentation of ImageColumns

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,21 @@
 import "./ImageList.css";
-import { ImageCard } from "./ImageCard";
+
+import { ImageColumn } from "./ImageColumn";
 import React from "react";
 
-export const ImageList = props => {
+export const ImageList = ({ columns }) => {
+  const cols = columns.map((column, index) => (
+    <ImageColumn key={`col-${index}`} images={column} />
+  ));
+  return <div className="image-list">{cols}</div>;
+  /*
   const images = props.images.map(image => {
     const { id, description, urls } = image;
     return (
       <ImageCard alt={description} key={id} src={urls.regular} image={image} />
     );
   });
-
-  return <div className="image-list">{images}</div>;
+  
+  return <div className="image-list" />;
+  */
 };
