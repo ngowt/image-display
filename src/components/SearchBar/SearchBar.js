4f58f111d21138b22b0a0e280ad2536695286f0f
Wilton Ngo <wiltonngo@gmail.com>
2019-05-15 13:38:39 -0400

    Added SearchOptions to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { SearchOptions } from "../SearchOptions/SearchOptions";
 
 export class SearchBar extends React.Component {
   state = { term: "" };
@@ -8,18 +9,28 @@ export class SearchBar extends React.Component {
     this.props.onSubmit(this.state.term);
   };
 
+  onSettingChangeHandler = val => {
+    console.log(val);
+  };
+
   render() {
     return (
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Image Search</label>
+          <div className="ui fluid action input">
             <input
               type="text"
               placeholder="Enter a word..."
               value={this.state.term}
               onChange={e => this.setState({ term: e.target.value })}
             />
+            <SearchOptions onSettingChangeEvent={this.onSettingChangeHandler} />
+            <button
+              className="ui button primary"
+              onClick={e => this.onFormSubmit(e)}
+            >
+              Search
+            </button>
           </div>
         </form>
       </div>
