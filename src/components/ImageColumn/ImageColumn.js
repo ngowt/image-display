d0220f90bbfb68bae485a3d95a3969faec161d77
Wilton Ngo <wiltonngo@gmail.com>
2019-05-15 08:49:35 -0400

    Deleted unutilized properties passed to ImageCard component

--- src/components/ImageColumn/ImageColumn.js
+++ src/components/ImageColumn/ImageColumn.js
@@ -4,15 +4,7 @@ import "./ImageColumn.css";
 
 export const ImageColumn = ({ images }) => {
   const imgCards = images.map(image => {
-    const { id, alt_description, urls } = image;
-    return (
-      <ImageCard
-        alt={alt_description}
-        key={id}
-        src={urls.regular}
-        image={image}
-      />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="img__col">{imgCards}</div>;
 };
