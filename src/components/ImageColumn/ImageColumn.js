2fa24f50476adca2657ff16eb7c71922b32d3366
Wilton Ngo <wiltonngo@gmail.com>
2019-05-15 23:56:54 -0400

    Now passes numColumns down to imageCard

--- src/components/ImageColumn/ImageColumn.js
+++ src/components/ImageColumn/ImageColumn.js
@@ -2,9 +2,9 @@ import React from "react";
 import { ImageCard } from "../ImageCard/ImageCard";
 import "./ImageColumn.css";
 
-export const ImageColumn = ({ images }) => {
+export const ImageColumn = ({ images, numColumns }) => {
   const imgCards = images.map(image => {
-    return <ImageCard key={image.id} image={image} />;
+    return <ImageCard key={image.id} image={image} numColumns={numColumns} />;
   });
-  return <div className="img__col">{imgCards}</div>;
+  return <div className={`img__col img__col-${numColumns}`}>{imgCards}</div>;
 };
