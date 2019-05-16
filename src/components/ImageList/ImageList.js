143c3e20e98228f408bb9f2de41e60a1b695c18c
Wilton Ngo <wiltonngo@gmail.com>
2019-05-15 23:57:58 -0400

    numColumns now passed down to ImageColumns

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -2,9 +2,9 @@ import React from "react";
 import { ImageColumn } from "../ImageColumn/ImageColumn";
 import "./ImageList.css";
 
-export const ImageList = ({ columns }) => {
-  const cols = columns.map((column, index) => (
-    <ImageColumn key={`col-${index}`} images={column} />
+export const ImageList = ({ imageColumns, numColumns }) => {
+  const cols = imageColumns.map((column, index) => (
+    <ImageColumn key={`col-${index}`} images={column} numColumns={numColumns} />
   ));
   return <div className="image-list">{cols}</div>;
 };
