2bc99c077b2806bce634d5b4dc52103cd66215d3
Wilton Ngo <wiltonngo@gmail.com>
2019-05-16 08:32:07 -0400

    Index now appended to ImageCard key to remove rare occurence of duplicates from endpoint

--- src/components/ImageColumn/ImageColumn.js
+++ src/components/ImageColumn/ImageColumn.js
@@ -3,8 +3,14 @@ import { ImageCard } from "../ImageCard/ImageCard";
 import "./ImageColumn.css";
 
 export const ImageColumn = ({ images, numColumns }) => {
-  const imgCards = images.map(image => {
-    return <ImageCard key={image.id} image={image} numColumns={numColumns} />;
+  const imgCards = images.map((image, index) => {
+    return (
+      <ImageCard
+        key={`${image.id}-${index}`}
+        image={image}
+        numColumns={numColumns}
+      />
+    );
   });
   return <div className={`img__col img__col-${numColumns}`}>{imgCards}</div>;
 };
