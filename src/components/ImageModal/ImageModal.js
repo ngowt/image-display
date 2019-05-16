df5dd4b12b5c0e2addad7373d3a3eabb911cfa58
Wilton Ngo <wiltonngo@gmail.com>
2019-05-16 12:39:48 -0400

    Added dummy statistics to Modal, will need to make another API call to get numDownloads and numViews

--- src/components/ImageModal/ImageModal.js
+++ src/components/ImageModal/ImageModal.js
@@ -1,54 +1,63 @@
 import React from "react";
-import { Button, Header, Image, Modal } from "semantic-ui-react";
+import { Button, Image, Modal, Icon, Statistic } from "semantic-ui-react";
+import "./ImageModal.css";
 
 export const ImageModal = ({ image, showModal, closeEvent }) => {
+  const { urls, user, likes } = image;
   console.log(image);
   return (
     <div>
       <Modal dimmer="blurring" open={showModal} onClose={closeEvent} closeIcon>
-        <Modal.Header>Select a Photo</Modal.Header>
+        <Modal.Header className="modal__header">
+          <a
+            href={user ? user.links.html : ""}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              floated="left"
+              size="mini"
+              src={user ? user.profile_image.small : ""}
+              style={{ borderRadius: "50%" }}
+            />
+          </a>
+          <Modal.Header>
+            <a
+              href={user ? user.links.html : ""}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {user ? user.name : ""}
+            </a>
+          </Modal.Header>
+        </Modal.Header>
         <Modal.Content image>
-          <Image wrapped src={image ? image.urls.regular : ""} />
-        </Modal.Content>
-        <Modal.Content>
-          <Modal.Description>
-            <Header>Modal Header</Header>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-              velit, sed quia non numquam eius modi tempora incidunt ut labore
-              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-              veniam, quis nostrum exercitationem ullam corporis suscipit
-              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
-              vel eum iure reprehenderit qui in ea voluptate velit esse quam
-              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-              voluptas nulla pariatur?"
-            </p>
-          </Modal.Description>
+          <Image wrapped src={urls ? urls.regular : ""} />
         </Modal.Content>
         <Modal.Actions>
-          <Button color="black" onClick={closeEvent}>
-            Nope
-          </Button>
-          <Button
-            positive
-            icon="checkmark"
-            labelPosition="right"
-            content="Yep, that's me"
-            onClick={closeEvent}
-          />
+          <Statistic.Group>
+            <Statistic>
+              <Statistic.Value>
+                <Icon name="heart" />
+                {likes}
+              </Statistic.Value>
+              <Statistic.Label>likes</Statistic.Label>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>
+                <Icon name="download" />
+                31,200
+              </Statistic.Value>
+              <Statistic.Label>Downloads</Statistic.Label>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>
+                <Icon name="eye" />
+                628
+              </Statistic.Value>
+              <Statistic.Label>Views</Statistic.Label>
+            </Statistic>
+          </Statistic.Group>
         </Modal.Actions>
       </Modal>
     </div>
