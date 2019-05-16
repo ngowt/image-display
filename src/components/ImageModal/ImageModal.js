b4947f11d20841a1af4bf775fa983b246720284d
Wilton Ngo <wiltonngo@gmail.com>
2019-05-16 10:56:07 -0400

    Added test modal

--- src/components/ImageModal/ImageModal.js
+++ src/components/ImageModal/ImageModal.js
@@ -1,48 +1,56 @@
 import React from "react";
 import { Button, Header, Image, Modal } from "semantic-ui-react";
 
-export class ImageModal extends React.Component {
-  state = { open: this.props.open };
-
-  show = dimmer => () => this.setState({ dimmer, open: true });
-  close = () => this.setState({ open: false });
-
-  render() {
-    const { open, dimmer } = this.state;
-    return (
-      <div>
-        <Button onClick={this.show("blurring")}>Blurring</Button>
-        <Modal dimmer={dimmer} open={open} onClose={this.close}>
-          <Modal.Header>Select a Photo</Modal.Header>
-          <Modal.Content image>
-            <Image
-              wrapped
-              size="medium"
-              src="/images/avatar/large/rachel.png"
-            />
-            <Modal.Description>
-              <Header>Default Profile Image</Header>
-              <p>
-                We've found the following gravatar image associated with your
-                e-mail address.
-              </p>
-              <p>Is it okay to use this photo?</p>
-            </Modal.Description>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button color="black" onClick={this.close}>
-              Nope
-            </Button>
-            <Button
-              positive
-              icon="checkmark"
-              labelPosition="right"
-              content="Yep, that's me"
-              onClick={this.close}
-            />
-          </Modal.Actions>
-        </Modal>
-      </div>
-    );
-  }
-}
+export const ImageModal = ({ image, showModal, closeEvent }) => {
+  console.log(image);
+  return (
+    <div>
+      <Modal dimmer="blurring" open={showModal} onClose={closeEvent} closeIcon>
+        <Modal.Header>Select a Photo</Modal.Header>
+        <Modal.Content image>
+          <Image wrapped src={image ? image.urls.regular : ""} />
+        </Modal.Content>
+        <Modal.Content>
+          <Modal.Description>
+            <Header>Modal Header</Header>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur?"
+            </p>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="black" onClick={closeEvent}>
+            Nope
+          </Button>
+          <Button
+            positive
+            icon="checkmark"
+            labelPosition="right"
+            content="Yep, that's me"
+            onClick={closeEvent}
+          />
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
+};
