0960f83328fc929d379464663b675fbeeb258911
Wilton Ngo <wiltonngo@gmail.com>
2019-05-17 09:44:07 -0400

    ImageModal now class based component, refactored statistics into own component

--- src/components/ImageModal/ImageModal.js
+++ src/components/ImageModal/ImageModal.js
@@ -1,78 +1,79 @@
 import React from "react";
-import { Button, Image, Modal, Icon, Statistic } from "semantic-ui-react";
-import "./ImageModal.css";
+import { unsplash } from "../../api/unsplash";
+import { Button, Image, Modal, Icon } from "semantic-ui-react";
+import { ImageStats } from "../ImageStats/ImageStats";
 
-export const ImageModal = ({ image, showModal, closeEvent }) => {
-  const { urls, user, likes, links } = image;
-  return (
-    <div>
-      <Modal dimmer="blurring" open={showModal} onClose={closeEvent} closeIcon>
-        <Modal.Header className="modal__header">
-          <a
-            href={user ? user.links.html : ""}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              floated="left"
-              size="mini"
-              src={user ? user.profile_image.small : ""}
-              style={{ borderRadius: "50%" }}
-            />
-          </a>
-          <Modal.Header>
-            <a
-              href={user ? user.links.html : ""}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {user ? user.name : ""}
+export class ImageModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: this.props.image.likes,
+      downloads: 0,
+      views: 0
+    };
+  }
+  componentDidMount = async () => {
+    const { id } = this.props.image;
+    const response = await unsplash.get(`/photos/${id}/statistics`);
+    this.setState({
+      likes: response.data.likes.total,
+      downloads: response.data.downloads.total,
+      views: response.data.views.total
+    });
+  };
+
+  render = () => {
+    const { image, showModal, closeEvent } = this.props;
+    const { urls, user, links } = image;
+    return (
+      <div>
+        <Modal
+          dimmer="blurring"
+          open={showModal}
+          onClose={closeEvent}
+          closeIcon
+        >
+          <Modal.Header className="modal__header">
+            <a href={user.links.html} target="_blank" rel="noopener noreferrer">
+              <Image
+                floated="left"
+                size="mini"
+                src={user.profile_image.small}
+                style={{ borderRadius: "50%" }}
+              />
             </a>
+            <Modal.Header>
+              <a
+                href={user.links.html}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {user.name}
+              </a>
+            </Modal.Header>
           </Modal.Header>
-        </Modal.Header>
-        <Modal.Content image>
-          <Image wrapped src={urls ? urls.regular : ""} />
-        </Modal.Content>
-        <Modal.Content>
-          <Statistic.Group>
-            <Statistic>
-              <Statistic.Value>
-                <Icon name="heart" />
-                {likes}
-              </Statistic.Value>
-              <Statistic.Label>likes</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>
-                <Icon name="download" />
-                31,200
-              </Statistic.Value>
-              <Statistic.Label>Downloads</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>
-                <Icon name="eye" />
-                628
-              </Statistic.Value>
-              <Statistic.Label>Views</Statistic.Label>
-            </Statistic>
-          </Statistic.Group>
-        </Modal.Content>
-        <Modal.Actions>
-          <a
-            href={links ? links.download : ""}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button color="green" onClick={closeEvent} inverted>
-              <Icon name="download" /> Download
+          <Modal.Content image>
+            <Image wrapped src={urls.regular} />
+          </Modal.Content>
+          <Modal.Content>
+            <ImageStats
+              likes={this.state.likes}
+              downloads={this.state.downloads}
+              views={this.state.views}
+            />
+          </Modal.Content>
+          <Modal.Actions>
+            <a href={links.download} target="_blank" rel="noopener noreferrer">
+              <Button color="green" onClick={closeEvent} inverted>
+                <Icon name="download" /> Download
+              </Button>
+            </a>
+            <Button color="red" onClick={closeEvent} inverted>
+              <Icon name="remove" /> Close
             </Button>
-          </a>
-          <Button color="red" onClick={closeEvent} inverted>
-            <Icon name="remove" /> Close
-          </Button>
-        </Modal.Actions>
-      </Modal>
-    </div>
-  );
-};
+          </Modal.Actions>
+        </Modal>
+      </div>
+    );
+  };
+}
