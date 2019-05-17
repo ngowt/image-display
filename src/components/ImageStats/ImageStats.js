930c08598bbba7e436729f63adc5796f91cbb8dc
Wilton Ngo <wiltonngo@gmail.com>
2019-05-17 19:00:47 -0400

    ImageStats now render stat changes

--- src/components/ImageStats/ImageStats.js
+++ src/components/ImageStats/ImageStats.js
@@ -1,31 +1,57 @@
 import React from "react";
-import { Icon, Statistic } from "semantic-ui-react";
+import { Icon, Statistic, StatisticGroup } from "semantic-ui-react";
 import numeral from "numeral";
+import "./ImageStats.css";
 
-export const ImageStats = ({ likes, downloads, views }) => {
+export const ImageStats = ({ statistics }) => {
+  const { likes, downloads, views } = statistics;
   return (
-    <Statistic.Group horizontal widths="three">
-      <Statistic>
-        <Statistic.Value color="red">
-          <Icon name="heart" color="red" />
-          {numeral(likes).format("0,0")}
-        </Statistic.Value>
-        <Statistic.Label>likes</Statistic.Label>
-      </Statistic>
-      <Statistic>
-        <Statistic.Value color="green">
-          <Icon name="download" color="green" />
-          {numeral(downloads).format("0,0")}
-        </Statistic.Value>
-        <Statistic.Label>downloads</Statistic.Label>
-      </Statistic>
-      <Statistic>
-        <Statistic.Value color="blue">
-          <Icon name="eye" color="blue" />
-          {numeral(views).format("0,0")}
-        </Statistic.Value>
-        <Statistic.Label>views</Statistic.Label>
-      </Statistic>
-    </Statistic.Group>
+    <div>
+      <StatisticGroup widths="two">
+        <Statistic>
+          <Statistic.Value className="basestat">
+            <Icon name="heart" color="red" />
+            {numeral(likes.total).format("0,0")}
+          </Statistic.Value>
+          <Statistic.Label className="basestat">likes</Statistic.Label>
+        </Statistic>
+        <Statistic color={likes.change > 0 ? "green" : "grey"}>
+          <Statistic.Value className="stat-change">
+            <Icon name="long arrow alternate up" color={"green"} />
+            {numeral(likes.change).format("0,0")}
+          </Statistic.Value>
+        </Statistic>
+      </StatisticGroup>
+      <StatisticGroup widths="two">
+        <Statistic>
+          <Statistic.Value className="basestat">
+            <Icon name="download" color="green" />
+            {numeral(downloads.total).format("0,0")}
+          </Statistic.Value>
+          <Statistic.Label className="basestat">downloads</Statistic.Label>
+        </Statistic>
+        <Statistic color={downloads.change > 0 ? "green" : "grey"}>
+          <Statistic.Value className="stat-change">
+            <Icon name="long arrow alternate up" color="green" />
+            {numeral(downloads.change).format("0,0")}
+          </Statistic.Value>
+        </Statistic>
+      </StatisticGroup>
+      <StatisticGroup widths="two">
+        <Statistic>
+          <Statistic.Value className="basestat">
+            <Icon name="eye" color="blue" />
+            {numeral(views.total).format("0,0")}
+          </Statistic.Value>
+          <Statistic.Label className="basestat">views</Statistic.Label>
+        </Statistic>
+        <Statistic color={views.change > 0 ? "green" : "grey"}>
+          <Statistic.Value className="stat-change">
+            <Icon name="long arrow alternate up" color="green" />
+            {numeral(views.change).format("0,0")}
+          </Statistic.Value>
+        </Statistic>
+      </StatisticGroup>
+    </div>
   );
 };
