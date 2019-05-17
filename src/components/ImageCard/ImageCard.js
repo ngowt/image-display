084107f6b2bee9db06590629a7dbf5fbe82ea28d
Wilton Ngo <wiltonngo@gmail.com>
2019-05-17 17:49:25 -0400

    Added instagram handle to card header

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { unsplash } from "../../api/unsplash";
+import { Comment } from "semantic-ui-react";
 import "./ImageCard.css";
 
 export const ImageCard = ({ image, numColumns, onImageClickedEvent }) => {
-  const { alt_description, urls, likes, user, color } = image;
+  const { alt_description, urls, likes, user } = image;
 
   const onImageClickedHandler = image => {
     onImageClickedEvent(image);
@@ -17,7 +18,35 @@ export const ImageCard = ({ image, numColumns, onImageClickedEvent }) => {
   return (
     <div className="img-card__div">
       <div className={`ui card img-card__div-${numColumns}`}>
-        <div className="content" style={{ backgroundColor: color }}>
+        <div className="content">
+          <Comment.Group>
+            <Comment>
+              <Comment.Avatar
+                src={user.profile_image.small}
+                href={user.links.html}
+                target="_blank"
+              />
+              <Comment.Content>
+                <Comment.Author href={user.links.html} target="_blank">
+                  {user.name}
+                </Comment.Author>
+                <Comment.Text>
+                  <a
+                    href={`https://www.instagram.com/${
+                      user.instagram_username
+                    }`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {user.instagram_username
+                      ? `@${user.instagram_username}`
+                      : ""}
+                  </a>
+                </Comment.Text>
+              </Comment.Content>
+            </Comment>
+          </Comment.Group>
+          {/*
           <a href={user.links.html} target="_blank" rel="noopener noreferrer">
             <img
               className="ui avatar image"
@@ -28,6 +57,7 @@ export const ImageCard = ({ image, numColumns, onImageClickedEvent }) => {
           <a href={user.links.html} target="_blank" rel="noopener noreferrer">
             {user.name}
           </a>
+          */}
         </div>
         <div className="image">
           <img
